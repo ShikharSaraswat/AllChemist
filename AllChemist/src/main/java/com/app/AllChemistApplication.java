40d78d70acd8ea9f64725e9beb1765f840c80277
Admin <Admin@DESKTOP-CFVM5GG>
2021-03-01 14:42:32 +0530

    This is the Base version with just the project structure with a configured resource file named :- application.properties.

---
+++ AllChemist/src/main/java/com/app/AllChemistApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllChemistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllChemistApplication.class, args);
+	}
+
+}
